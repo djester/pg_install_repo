9dfefb5d1537568bb2ba7dc59da2404c6caf04d8
djester <djester@mail.ru>
2015-12-25 13:26:43 +0300

    lsyncd cmake use

--- pg_run_install.sh
+++ pg_run_install.sh
@@ -4,9 +4,9 @@
 # Be carreful!!! libevent is dependency for tmux and pgbouncer
 #
 
-./scripts/libevent_install.sh
-./scripts/tmux_install.sh
-./scripts/pg_install.sh
+#./scripts/libevent_install.sh
+#./scripts/tmux_install.sh
+#./scripts/pg_install.sh
 ./scripts/pgb_install.sh
 ./scripts/lsyncd_install.sh
 
--- scripts/lsyncd_install.sh
+++ scripts/lsyncd_install.sh
@@ -2,16 +2,18 @@
 
 mkdir -p /opt/src & cd /opt/src 
 
+yum install -y cmake lua lua-devel || exit 1
+
 lsyncd_ver=2.1.6 
-lsyncd_name=lsyncd-${lsyncd_ver}
+lsyncd_name=lsyncd-release-${lsyncd_ver}
 lsyncd_nametar=${lsyncd_name}.tar.gz
+
 if [ ! -f $lsyncd_nametar ]; then
-    wget -O $lsyncd_nametar -c https://github.com/axkibe/lsyncd/archive/release-${lsyncd_ver}.tar.gz || ( echo "ERR: $lsyncd_nametar not found!!!"; exit 1 )
+    wget -O $lsyncd_nametar -c https://github.com/axkibe/lsyncd/archive/release-${lsyncd_ver}.tar.gz || ( echo "ERROR: $lsyncd_nametar not found!!!"; exit 1 )
 fi
 tar -zxf $lsyncd_nametar && 
 (
-cd $lsyncd_name &&
-./configure --prefix=/opt/$lsyncd_name && make && ( [ `id -un` = "root" ] && make install || sudo make install ) && ln -s /opt/$lsyncd_name /opt/lsyncd
+export DESTDIR="/opt/$lsyncd_name" && cd $lsyncd_name && cmake . && make && make install  && ln -s /opt/$lsyncd_name /opt/lsyncd
 ) || exit 1
  
 mkdir -p /var/log/lsyncd
