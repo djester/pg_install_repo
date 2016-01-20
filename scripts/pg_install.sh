6ba4b772ea4d6545ffa8d22812783b31be6a9e5b
djester <djester@mail.ru>
2016-01-20 13:51:38 +0300

    user PG_HOME directory exists checking

--- scripts/pg_install.sh
+++ scripts/pg_install.sh
@@ -46,6 +46,7 @@ read -s -n 1
 # Create group with system GUID
 groupadd -r postgres
 # Create user with system UID and with home directory
+[ -d $PG_HOME ] || mkdir -p $PG_HOME || exit 1
 useradd -r -g postgres -s /bin/bash -d $PG_HOME/postgres -m -k /etc/skel postgres || exit 1
 mkdir -p $PGDATA $PGLOG $PGARCH $PGWAL
 chown -R postgres:postgres $PGDATA $PGLOG $PGWAL
