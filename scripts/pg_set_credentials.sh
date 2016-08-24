a5de12e57b97295c86e52ac8a793c04639e7ffa4
djester <djester@mail.ru>
2016-08-24 15:52:08 +0300

    fix error handle

--- scripts/pg_set_credentials.sh
+++ scripts/pg_set_credentials.sh
@@ -2,8 +2,8 @@
 
 echo "Set credentials on folders "
 
-mkdir -p $PGDATA $PGLOG $PGARCH $PGWAL
-chown -R postgres:postgres $PGDATA $PGARCH $PGLOG $PGWAL
+mkdir -p $PGDATA $PGLOG $PGARCH $PGWAL && \
+chown -R postgres:postgres $PGDATA $PGARCH $PGLOG $PGWAL && \
 chmod 0700 $PGDATA || exit 1
 
 exit 0
