e3d0deb0f03463e0c8a63729be1a6b3b69a46f50
djester <djester@mail.ru>
2016-08-24 12:46:44 +0300

    Separate pg_user_create.sh and pg_set_credentials.sh

--- scripts/pg_user_create.sh
+++ scripts/pg_user_create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Create user postgres with credentials"
+echo "Create group & user postgres"
 
 # Create group with system GUID
 groupadd -r postgres || exit 1
@@ -8,10 +8,6 @@ groupadd -r postgres || exit 1
 # Create user with system UID and with home directory
 [ -d $PG_HOME ] || mkdir -p $PG_HOME || exit 1
 useradd -r -g postgres -s /bin/bash -d $PG_HOME/postgres -m -k /etc/skel postgres || exit 1
-
-mkdir -p $PGDATA $PGLOG $PGARCH $PGWAL
-chown -R postgres:postgres $PGDATA $PGARCH $PGLOG $PGWAL
-chmod 0700 $PGDATA || exit 1
 
 exit 0
 
