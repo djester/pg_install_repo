31eb8064dd4d3746a575153f49cfddf4ddc313da
djester <djester@mail.ru>
2016-09-21 17:22:07 +0300

    set tmux version in command-line; remove libevent_install.conf dependency

--- scripts/tmux_install.sh
+++ scripts/tmux_install.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+#
+# Example of usage:
+# ./tmux_install.sh 2.2
+#
 
 mkdir -p /opt/src
 
 #Dependences
 yum -y install ncurses-devel || exit1
 
-
 #Install
 cd /opt/src
 
-tmux_ver=2.2
+tmux_ver=${tmux_ver:-$1}
+[ "${tmux_ver}" ] || { echo "ERROR: Varible tmux_ver is not defined!" >&2; exit 1; }
+
 tmux_name=tmux-$tmux_ver
 tmux_nametar=${tmux_name}.tar.gz
+
 if [ ! -f $tmux_nametar ]; then
    wget -O ${tmux_nametar} "https://github.com/tmux/tmux/releases/download/${tmux_ver}/${tmux_nametar}" || exit 1
 fi
+
 tar zxf ${tmux_nametar} && 
 (
-  cd ${tmux_name} && PKG_CONFIG_PATH=/opt/${libevent_name1}/lib/pkgconfig ./configure --prefix=/opt/${tmux_name} && make && make install && ln -s /opt/${tmux_name} /opt/tmux
+  cd ${tmux_name} && PKG_CONFIG_PATH=/opt/libevent/lib/pkgconfig ./configure --prefix=/opt/${tmux_name} && make && make install && ln -s /opt/${tmux_name} /opt/tmux
 ) || exit 1
 
 # Configuration file
