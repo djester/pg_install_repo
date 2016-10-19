58d5b82906a9bb4f44f60c514d70547f9c2642ba
djester <djester@mail.ru>
2016-10-19 11:38:14 +0300

    Add create extensions

--- pg_post_install.sh
+++ pg_post_install.sh
@@ -57,6 +57,9 @@ echo "-- Create empty DB for monitoring agent connection"
 su - postgres -c "createdb -U postgres zabbix" || exit 1
 echo "-- Create user for monitoring agent connection"
 su - postgres -c "createuser -U postgres zabbix" || exit 1
+echo "-- Create postgres extention in for monitoring db"
+su - postgres -c "psql -qAtX -U postgres -d zabbix -c 'create extension pg_stat_statements'" || exit 1
+su - postgres -c "psql -qAtX -U postgres -d zabbix -c 'create extension pg_buffercache'" || exit 1
 echo "-- Create Replication user"
 su - postgres -c "createuser -U postgres -SDR --replication replica" || exit 1
 
