21d99e94cc8d21965b2b936c6ebc64ebb27bb8bf
djester <djester@mail.ru>
2016-10-19 15:09:33 +0300

    Add create application db owner role

--- pg_post_install.sh
+++ pg_post_install.sh
@@ -17,34 +17,25 @@ echo Copy init-scripts:
 
 echo -- PostgreSQL
 cp ${repo}/init.d/postgresql-${PG_REL} /etc/init.d/postgresql && chkconfig postgresql on || exit 1
-
 echo -- lsyncd
 cp ${repo}/init.d/lsyncd /etc/init.d/lsyncd && chkconfig lsyncd on || exit 1
-
 echo -- pgbouncer
 cp ${repo}/init.d/pgbouncer /etc/init.d/pgbouncer && chkconfig pgbouncer on || exit 1
-
 
 echo Copy configs
 
 echo "-- sysctl.conf (DON'T FORGET EDIT IT!!!)"
 cp ${repo}/etc/sysctl.conf /etc/sysctl.conf && sysctl -p || exit 1
-
 echo "-- limits.conf"
 cp ${repo}/etc/limits.conf /etc/security/limits.conf || exit 1
-
 echo "-- pg_hba.conf (DON'T FORGET EDIT IT!!!)"
 cp ${repo}/etc/pg_hba.conf ${PGSQL}/pg_hba.conf || exit 1
-
 echo "-- postgresql.conf"
 cp ${repo}/etc/postgresql.conf ${PGDATA}/postgresql.conf && chown postgres:postgres ${PGDATA}/postgresql.conf && chmod 600 ${PGDATA}/postgresql.conf || exit 1
-
 echo "-- postgres crontab"
 cp ${repo}/etc/pgsql.cron /var/spool/cron/postgres || exit 1
-
 echo "-- recovery.conf.tmpl (DON'T FORGET EDIT IT!!!)"
 cp ${repo}/etc/recovery.conf.tmpl ${PGSQL}/recovery.conf.tmpl || exit 1
-
 echo "-- postgres lsyncd.conf"
 cp ${repo}/etc/lsyncd.conf /etc/lsyncd.conf || exit 1
 
@@ -62,4 +53,5 @@ su - postgres -c "psql -qAtX -U postgres -d zabbix -c 'create extension pg_stat_
 su - postgres -c "psql -qAtX -U postgres -d zabbix -c 'create extension pg_buffercache'" || exit 1
 echo "-- Create Replication user"
 su - postgres -c "createuser -U postgres -SDR --replication replica" || exit 1
-
+echo "-- Create owner role for appliction db"
+su - postgres -c "psql -qAtX -U postgres -c 'create role app_owner with nologin'" || exit 1
