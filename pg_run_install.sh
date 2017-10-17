013dc63245048776eed5ea7669f27bee7419ea35
djester <djester@mail.ru>
2017-10-17 10:04:37 +0300

    check main script parameters

--- pg_run_install.sh
+++ pg_run_install.sh
@@ -39,11 +39,27 @@ if test $# -gt 0 ; then
     # init variables
 
     TARGET_DIR=$1
+    [ "${TARGET_DIR}" ] || { echo "ERR: Varible TARGET_DIR is not defined!" >&2; exit 1; }
     export TARGET_DIR
+
     PG_VER=$2
+    [ "${PG_VER}" ] || { echo "ERR: Varible PG_VER is not defined!" >&2; exit 1; }
+
     PG_REL=${PG_VER%.*}
     PG_DATA=${TARGET_DIR}/pgsql/${PG_REL}
     PG_LC=ru_RU.UTF-8
+
+    TMUX_INST="YES"
+    PG_INST="YES"
+    USER_CREATE="YES"
+    DB_INIT="YES"
+    PGB_INST="YES"
+    SYNC_INST="YES"
+ 
+    if [ TMUX_INST == "YES" ] || [ PGB_INST == "YES" ] || [ SYNC_INST == "YES" ];
+    then
+        LIB_EVENT_INST="YES"
+    fi
 
     TMUX_VER=2.4
     PGB_VER=1.7.2
