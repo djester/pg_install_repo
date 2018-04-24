9250a84db269a02a1fe3b9cbf1d8d6041834d9f4
djester <djester@mail.ru>
2018-04-24 12:45:31 +0300

    minimal fix variable style

--- scripts/pg_install.sh
+++ scripts/pg_install.sh
@@ -3,7 +3,7 @@
 PG_VER=${PG_VER:-$1}
 [ "${PG_VER}" ] || { echo "ERR: Varible PG_VER is not defined!" >&2; exit 1; }
 
-echo "Install PostgreSQL $PG_VER"
+echo "Install PostgreSQL ${PG_VER}"
 
 mkdir -p /opt/src && cd /opt/src || exit 1
 
@@ -13,32 +13,35 @@ yum -y remove postgresql-libs postgresql postgresql-odbc
 # install dependencies
 yum install -y readline-devel libxml2-devel libxslt-devel || exit 1
  
-pg_name=postgresql-$PG_VER
-pg_nametar=$pg_name.tar.gz
+PG_NAME=postgresql-${PG_VER}
+PG_TAR=${PG_NAME}.tar.gz
+PG_PATH=/opt/${PG_NAME}
+PG_SLINK=/opt/postgresql
+PG_URL=https://ftp.postgresql.org/pub/source/v${PG_VER}/${PG_TAR}
 
-if [ -e /opt/$pg_name ]; then
-  echo "/opt/$pg_name exists. May be PostgreSQL $PG_VER software was installed before..."
+if [ -e ${PG_PATH} ]; then
+  echo "${PG_PATH} exists. May be PostgreSQL ${PG_VER} software was installed before..."
   exit 1
 fi
 
-if [ ! -f $pg_nametar ]; then
-    wget -c https://ftp.postgresql.org/pub/source/v${PG_VER}/postgresql-${PG_VER}.tar.gz || exit 1
+if [ ! -f ${PG_TAR} ]; then
+    wget -c ${PG_URL} || exit 1
 fi
 
-tar -zxf $pg_nametar && \
-cd $pg_name && \
-./configure --prefix=/opt/$pg_name with_libxml=yes with_libxslt=yes && \
+tar -zxf ${PG_TAR} && \
+cd ${PG_NAME} && \
+./configure --prefix=${PG_PATH} with_libxml=yes with_libxslt=yes && \
 make world -j6 && [ `id -un` = "root" ] && \
 make install-world || sudo make install-world || exit 1
 
-if [ -e /opt/postgresql ];
+if [ -e ${PG_SLINK} ];
 then
-  echo "/opt/postgresql exists. May be PostgreSQL software was installed before..."
+  echo "${PG_SLINK}  exists. May be PostgreSQL software was installed before..."
   ls -lh /opt/postgresql
-  echo "If you want upgrade software, relink it to $pg_name"
+  echo "If you want upgrade software, relink it to ${PG_NAME}"
   exit 1
 else
-  ln -s $pg_name /opt/postgresql || exit 1
+  ln -s ${PG_PATH} ${PG_SLINK}  || exit 1
 fi
 
 # set PATH to profile enviroment
