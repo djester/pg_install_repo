4f57a6fd3e1eda127de1575a13f3bbd198b774e4
djester <djester@mail.ru>
2019-01-16 10:33:04 +0300

    add data-checksums option to init db command

--- scripts/pg_db_init.sh
+++ scripts/pg_db_init.sh
@@ -6,7 +6,7 @@ PG_ENCODING=${PG_LC##*.}
 
 echo "Initialize DB server"
 
-cr_db_cmd="/opt/postgresql/bin/initdb -D ${PG_DATA} -E ${PG_ENCODING} --locale=${PG_LC} --lc-collate=${PG_LC} --lc-ctype=${PG_LC}"
+cr_db_cmd="/opt/postgresql/bin/initdb -D ${PG_DATA} -E ${PG_ENCODING} --locale=${PG_LC} --lc-collate=${PG_LC} --lc-ctype=${PG_LC} -k"
 su postgres -c "${cr_db_cmd}" || exit 1
 
 exit 0
