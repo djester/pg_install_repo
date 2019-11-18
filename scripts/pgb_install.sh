0e5497d56ef427e35b96648bd024d34dd0174912
djester <djester@mail.ru>
2019-11-18 12:50:24 +0300

    Revert to 4063d62

--- scripts/pgb_install.sh
+++ scripts/pgb_install.sh
@@ -11,42 +11,37 @@ yum -y install c-ares c-ares-devel
 # asquire version from command-line parameter or pre-defined variable
 PGB_VER=${PGB_VER:-$1}
 [ "${PGB_VER}" ] || { echo "ERROR: Varible PGB_VER is not defined!" >&2; exit 1; }
-PGB_GIT_TAG=$(echo ${PGB_VER} | sed 's/^/postgres_/;s/\./_/g')
 
 PGB_NAME=pgbouncer-${PGB_VER}
+PGB_TAR=${PGB_NAME}.tar.gz
 PGB_PATH=/opt/${PGB_NAME}
 PGB_SLINK=/opt/pgbouncer
+PGB_URL=https://pgbouncer.github.io/downloads/files/${PGB_VER}/${PGB_TAR}
 
 if [ -e ${PGB_PATH} ]; then
   echo "${PGB_PATH} exists. May be pgbouncer ${PGB_VER} software was installed before..."
   exit 1
 fi
 
-# build from git 
-cd /git
-git clone https://github.com/pgbouncer/pgbouncer.git
-cd pgbouncer/
-git checkout tags/$PGB_GIT_TAG
+# download sources
+if [ ! -f ${PGB_TAR} ]; then
+    wget -c ${PGB_URL} || exit 1
+fi
 
-<<<<<<< HEAD
 # unpack sources and build software 
 tar -zxf ${PGB_TAR} && 
 (
 cd ${PGB_NAME} &&
+    git submodule init && \
+    git submodule update && \
+    ./autogen.sh && \
     ./configure --prefix=${PGB_PATH} --with-libevent=/opt/libevent --with-cares && \
     make && ( [ `id -un` = "root" ] && make install || sudo make install ) 
 ) || exit 1
-=======
-git submodule init && \
-git submodule update && \
-./autogen.sh && \
-./configure --prefix=${PGB_PATH} --with-libevent=/opt/libevent --with-cares && \
-make && ( [ `id -un` = "root" ] && make install || sudo make install ) 
->>>>>>> 23727984af5dc09789ba20e02ba96a9919d39a04
 
 if [ -e ${PGB_SLINK} ];
 then
-  echo "${PGB_SLINK}  exists. May be pgbouncer software was installed before..."
+  echo "${PGB_SLINK}  exists. May be PostgreSQL software was installed before..."
   ls -lh ${PGB_SLINK}
   echo "If you want upgrade software, relink it to ${PGB_NAME}"
   exit 1
